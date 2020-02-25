61091f0eb7904b2431e2f30a83e442ca6b549467
MITESH JHA <cool.miteshjha@gmail.com>
2020-02-25 08:45:49 +0530

    adding automapper 4.1.1 demo

---
+++ AutoMapper/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper
+{
+    class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            // mapping for which source and dest prop same
+            Mapper.CreateMap<Order, OrderDto>();
+
+            // mapping for which source and dest prop not same
+            Mapper.CreateMap<Employee, EmployeeDto>()
+                .ForMember(dest => dest.EmpName, opt => opt.MapFrom(src => src.name))
+                .ForMember(dest => dest.EmpId, opt => opt.MapFrom(src => src.id));
+
+        }
+    }
+}
---
+++ AutoMapper/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace AutoMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // install automapper from nuget 
+            // i have used 4.1.1
+            // automapper is two step process
+            // automapper internally use reflectio to see properties of type and map it
+            Mapper.Initialize(p => p.AddProfile<MappingProfile>()); // this should be in startup folder
+
+            Order o = new Order();
+            o.id = 1;
+            o.name = "order1";
+            o.amt = 2020;
+
+            OrderDto odto = Mapper.Map<OrderDto>(o);
+        }
+    }
+
+    class Order
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int amt { get; set; }
+    }
+
+    class OrderDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+
+    class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int amt { get; set; }
+    }
+
+    class EmployeeDto
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+    }
+
+}
