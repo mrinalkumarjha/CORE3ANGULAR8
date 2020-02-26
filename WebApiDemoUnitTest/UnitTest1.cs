f5ae756ec01689f8898262ae9c52e48ee583cacd
MITESH JHA <cool.miteshjha@gmail.com>
2020-02-26 07:58:00 +0530

    added unit test

---
+++ WebApiDemoUnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using EmployeeDataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiDemo.Controllers;
+
+namespace WebApiDemoUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            EmployeesController employeesController = new EmployeesController();
+            IEnumerable<Employee> emp;
+            emp = employeesController.Get();
+            //Assert.AreEqual(3, emp.());
+            //Assert.IsTrue(emp.Contains(1));
+            Assert.IsNull(emp);
+        }
+    }
+}
